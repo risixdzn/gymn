49bc3366f6bcae3a3aa586e856c563b919b2edd1
Ricardo Amorim <102877738+risixdzn@users.noreply.github.com>
2023-04-25 21:46:11 -0300

    :milky_way: (feature) Add Change Theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -11,4 +11,20 @@ const GymnDarkTheme = Themes.createFromDark({
     },
 })
 
-export default GymnDarkTheme
+const GymnLightTheme = Themes.createFromLight({
+  type: 'gymnTheme',
+  palette: {
+    background: '#ededed',
+    success: '#8a2be2',
+    successLight: '#8f3ddb',
+    purple: '#8a2be1',
+    alert: '#8a2be3',      
+  },
+})
+
+const GymnThemes ={
+  GymnDarkTheme,
+  GymnLightTheme
+}
+
+export default GymnThemes
