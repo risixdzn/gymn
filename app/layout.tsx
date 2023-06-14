c08cdc3d9d5811f63f635c02d4d5529f62249812
Ricardo Amorim <102877738+risixdzn@users.noreply.github.com>
2023-06-13 21:00:48 -0300

    :milky_way: (feature) Add theme changer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 
+import { ThemeProvider } from '@/components/ThemeProvider'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -15,7 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+          {children}
+        </ThemeProvider>        
+      </body>
     </html>
   )
 }
