18db3f93aa6b15c7bd3c399c113fd888a16ff786
Ricardo Amorim <102877738+risixdzn@users.noreply.github.com>
2023-07-08 17:06:08 -0300

    :recycle: (refactor) redesign register account type selection

--- components/Auth/Register/RegistrationFormProvider.tsx
+++ components/Auth/Register/RegistrationFormProvider.tsx
@@ -1,9 +1,16 @@
 "use client";
 
+import Image from "next/image";
 import { useState } from "react";
 import RegisterGymOwnerForm from "./RegisterGymOwnerForm";
 import RegisterMemberForm from "./RegisterMemberForm";
 import { Button } from "@/components/ui/button";
+import { Balancer } from "react-wrap-balancer";
+
+import OwnerActive from "../../../public/BenchPress_Active.svg";
+import OwnerMuted from "../../../public/BenchPress_Muted.svg";
+import MemberActive from "../../../public/WeightLift_Active.svg";
+import MemberMuted from "../../../public/WeightLift_Muted.svg";
 
 type AccountTypes = "member" | "gymOwner" | "";
 
@@ -27,41 +34,58 @@ export default function RegistrationFormProvider() {
                     </h3>
                     <div
                         onClick={() => setAccountType("member")}
-                        className={`w-full mt-4 bg-background py-5 px-7 flex gap-7 rounded-lg border-[3px] cursor-pointer transition-colors ${
+                        className={` min-h-[7rem] items-center relative select-none w-full mt-4 bg-background py-5 px-7 flex gap-7 rounded-lg border-[3px] cursor-pointer transition-colors ${
                             accountType == "member"
                                 ? "border-purple-600 bg-purple-600/10"
                                 : "border-border bg-background"
                         }`}
                     >
                         <input
+                            key={1}
                             type='radio'
                             value='member'
                             id='member'
+                            className='absolute top-5 right-5'
                             checked={accountType === "member"}
                             onChange={() => setAccountType("member")}
                         />
+                        <Image
+                            src={accountType == "member" ? MemberActive : MemberMuted}
+                            width={70}
+                            height={70}
+                            alt=''
+                            className='inline-block w-15'
+                        />{" "}
                         <div>
                             <label htmlFor='gymOwner' className='font-semibold tracking-tight'>
                                 Membro
                             </label>
                             <p className='max-w-[15rem] text-xs lg:text-sm text-muted-foreground'>
-                                Se você treina em uma academia e quer manter controle sobre seus
-                                treinos, selecione esta opção.
+                                <Balancer>Acompanhe seus treinos de forma eficiente.</Balancer>
                             </p>
                         </div>
                     </div>
                     <div
                         onClick={() => setAccountType("gymOwner")}
-                        className={`w-full mt-4 py-5 px-7 flex gap-7 rounded-lg border-[3px] transition-colors cursor-pointer ${
+                        className={`min-h-[7rem] items-center relative select-none w-full mt-4 py-5 px-7 flex gap-7 rounded-lg border-[3px] transition-colors cursor-pointer ${
                             accountType == "gymOwner"
                                 ? "border-purple-600 bg-purple-600/10"
                                 : "border-border bg-background"
                         }`}
                     >
+                        <Image
+                            src={accountType == "gymOwner" ? OwnerActive : OwnerMuted}
+                            width={70}
+                            height={70}
+                            alt=''
+                            className='inline-block w-15'
+                        />{" "}
                         <input
+                            key={2}
                             type='radio'
                             value='gymOwner'
                             id='gymOwner'
+                            className='absolute top-5 right-5'
                             checked={accountType === "gymOwner"}
                             onChange={() => setAccountType("gymOwner")}
                         />
@@ -70,8 +94,9 @@ export default function RegistrationFormProvider() {
                                 Dono de academia
                             </label>
                             <p className='max-w-[15rem] text-xs lg:text-sm text-muted-foreground'>
-                                Se você ja possui uma academia ou está criando uma, esta é a sua
-                                opção.
+                                <Balancer>
+                                    Gerencie seus alunos e seus treinos com facilidade.
+                                </Balancer>
                             </p>
                         </div>
                     </div>
