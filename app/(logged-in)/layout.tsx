cd80aa6d6f9cdd61cfa7b09c38b529ff0f4ba5da
Ricardo Amorim <102877738+risixdzn@users.noreply.github.com>
2023-07-26 21:11:36 -0300

    :dart: (fix) `Dashboard` Fix hydration errors

--- app/(logged-in)/layout.tsx
+++ app/(logged-in)/layout.tsx
@@ -1,7 +1,7 @@
 import "../globals.css";
 import { Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/toaster";
-
+import PagesContainer from "@/components/Dashboard/PagesContainer";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import Sidebar from "@/components/Dashboard/Sidebar/Sidebar";
 
@@ -17,8 +17,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <html lang='en'>
             <body className={inter.className}>
                 <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-                    <Sidebar>{children}</Sidebar>
+                    <Sidebar />
                     <Toaster />
+                    <PagesContainer>{children}</PagesContainer>
                 </ThemeProvider>
             </body>
         </html>
