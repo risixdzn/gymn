5c718d3f93f8af0c1a791094f4e4a4342a8d7889
Ricardo Amorim <102877738+risixdzn@users.noreply.github.com>
2023-08-20 23:03:38 -0300

    :milky_way: `Profile` feat: Edit profile redesign

--- tailwind.config.js
+++ tailwind.config.js
@@ -90,5 +90,8 @@ module.exports = {
             },
         },
     },
-    plugins: [require("tailwindcss-animate")],
+    plugins: [
+        require("tailwindcss-animate"),
+        require("tailwind-scrollbar")({ nocompatible: true }),
+    ],
 };
