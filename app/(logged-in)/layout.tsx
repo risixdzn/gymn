afd54e2ce327c7eb12c958e84e6ddc1e4fbf854b
Ricardo Amorim <102877738+risixdzn@users.noreply.github.com>
2023-09-01 23:50:04 -0300

    :dart: `Layout` fix: Remove query client to *try* fixing build error

--- app/(logged-in)/layout.tsx
+++ app/(logged-in)/layout.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "@/components/ThemeProvider";
 import DashUi from "@/components/Dashboard/DashUi";
 import { Analytics } from "@vercel/analytics/react";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+//import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,19 +17,17 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-    const reactQueryClient = new QueryClient();
+    //const reactQueryClient = new QueryClient();
 
     return (
         <html lang='en'>
             <body className={`${inter.className} select-none`}>
-                <QueryClientProvider client={reactQueryClient}>
-                    <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-                        <DashUi />
-                        <Toaster />
-                        <Analytics />
-                        <PagesContainer>{children}</PagesContainer>
-                    </ThemeProvider>
-                </QueryClientProvider>
+                <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+                    <DashUi />
+                    <Toaster />
+                    <Analytics />
+                    <PagesContainer>{children}</PagesContainer>
+                </ThemeProvider>
             </body>
         </html>
     );
