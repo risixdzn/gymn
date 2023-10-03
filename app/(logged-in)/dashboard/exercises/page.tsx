e8dedb02c4d0c669591fabda9f19e96b4b11182b
Ricardo Amorim <102877738+risixdzn@users.noreply.github.com>
2023-10-03 16:52:29 -0300

    :milky_way: `Exercises` feat: Refactor filters

--- app/(logged-in)/dashboard/exercises/page.tsx
+++ app/(logged-in)/dashboard/exercises/page.tsx
@@ -11,6 +11,7 @@ import Filters from "@/components/Dashboard/Exercises/Filters";
 import { Skeleton } from "@/components/ui/skeleton";
 import Link from "next/link";
 import AddExercise from "@/components/Dashboard/Exercises/AddExercise/AddExercise";
+import { useSearchParams } from "next/navigation";
 
 export type Exercise = {
     id: string;
@@ -22,34 +23,45 @@ export type Exercise = {
 };
 
 //TODO: Add 'remove filter' button, animating x to right, text to left
-
 export default function Exercises() {
-    const [filteredMuscle, setFilteredMuscle] = useState("");
-    const [filteredEquipment, setFilteredEquipment] = useState("");
-    const [currentFilters, setCurrentFilters] = useState<string[]>([]);
+    const searchParams = useSearchParams();
+
+    //Defines query params
+    const queryParams: Record<string, string | null> = {
+        muscle: searchParams.get("muscle"),
+        equipment: searchParams.get("equipment"),
+        difficulty: searchParams.get("level"),
+    };
+
+    // Filter out keys with null values
+    const filteredQueryParams = Object.entries(queryParams).reduce((acc, [key, value]) => {
+        if (value !== null) {
+            acc[key] = value;
+        }
+        return acc;
+    }, {} as { [key: string]: string });
+
+    const queryString = new URLSearchParams(filteredQueryParams);
 
     const { data, isLoading } = useQuery({
-        queryKey: ["exercises", filteredMuscle, filteredEquipment], //key and params to define the query
+        queryKey: ["exercises", queryParams.muscle, queryParams.equipment, queryParams.difficulty], //key and params to define the query
         queryFn: () => {
             //function called on querying
-            return axios.get(`/api/exercises?muscle=${filteredMuscle}`).then((res) => res.data);
+            return axios
+                .get(`/api/exercises?${queryString && queryString}`)
+                .then((res) => res.data);
         },
     });
 
     const renderTitle = () => {
-        if (filteredMuscle) return `Exercícios para ${filteredMuscle}.`;
-
-        if (filteredEquipment) return `Exercícios com ${filteredEquipment}`;
-
-        if (filteredMuscle && filteredEquipment) {
-            return `Exercicios para ${filteredMuscle} com ${filteredEquipment}`;
+        if (queryParams.muscle) return `Exercícios para ${queryParams.muscle}.`;
+        if (queryParams.equipment) return `Exercícios com ${queryParams.equipment}`;
+        if (queryParams.muscle && queryParams.equipment) {
+            return `Exercicios para ${queryParams.muscle} com ${queryParams.equipment}`;
         } else {
             return "Todos exercícios";
         }
     };
-
-    const [muscleFilterDrawerOpen, setMuscleFilterDrawerOpen] = useState(false);
-    const [equipmentFilterDrawerOpen, setEquipmentFilterDrawerOpen] = useState(false);
 
     const skeletons = new Array(15).fill(null);
 
@@ -66,18 +78,10 @@ export default function Exercises() {
                         Exercícios para todos grupos musculares
                     </p>
                 </div>
+                <p className='hidden'>{`/api/exercises?${queryString}`}</p>
             </div>
 
-            <Filters
-                className='mt-4'
-                filteredMuscle={filteredMuscle}
-                filteredEquipment={filteredEquipment}
-                muscleFilterDrawerOpen={muscleFilterDrawerOpen}
-                setMuscleFilterDrawerOpen={setMuscleFilterDrawerOpen}
-                setFilteredMuscle={setFilteredMuscle}
-                setCurrentFilters={setCurrentFilters}
-                currentFilters={currentFilters}
-            />
+            <Filters className='mt-4' />
 
             <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-2 md:gap-4 mt-4 mb-20 lg:mb-0'>
                 {!isLoading ? (
--- components/Dashboard/Exercises/Filters.tsx
+++ components/Dashboard/Exercises/Filters.tsx
@@ -6,6 +6,8 @@ import { Dispatch, SetStateAction } from "react";
 import { Badge } from "@/components/ui/badge";
 import { cn } from "@/lib/utils";
 import { motion } from "framer-motion";
+import FilterDrawer from "./FilterDrawer";
+import { useSearchParams } from "next/navigation";
 
 export const muscles = [
     "Abdominais",
@@ -29,103 +31,25 @@ export const muscles = [
 
 export const levels = ["Iniciante", "Intermediário", "Avançado"];
 
-type FiltersProps = {
-    className: string;
-    filteredMuscle: string;
-    filteredEquipment: string;
-    muscleFilterDrawerOpen: boolean;
-    setMuscleFilterDrawerOpen: Dispatch<SetStateAction<boolean>>;
-    setFilteredMuscle: Dispatch<SetStateAction<string>>;
-    setCurrentFilters: Dispatch<SetStateAction<string[]>>;
-    currentFilters: string[];
-};
+export default function Filters({ className }: { className: string }) {
+    const searchParams = useSearchParams();
+    const currentFilters = {
+        muscle: searchParams.get("muscle"),
+        equipment: searchParams.get("equipment"),
+        level: searchParams.get("level"),
+    };
 
-export default function Filters({
-    className,
-    filteredMuscle,
-    filteredEquipment,
-    muscleFilterDrawerOpen,
-    setMuscleFilterDrawerOpen,
-    setFilteredMuscle,
-    setCurrentFilters,
-    currentFilters,
-}: FiltersProps) {
     return (
-        <>
-            <div
-                id='filters'
-                className={cn("w-full flex flex-row flex-wrap gap-2 transition-all", className)}
-            >
-                {/* Badges */}
-                {filteredMuscle && (
-                    <motion.a
-                        initial={{ scale: 0.8, opacity: 0 }}
-                        animate={{ scale: 1, opacity: 1 }}
-                        className='cursor-pointer'
-                    >
-                        <Badge
-                            className='group  transition-all hover:pr-10 h-8 rounded-md text-sm px-4 gap-2 relative'
-                            variant={"secondary"}
-                            onClick={() => setFilteredMuscle("")}
-                        >
-                            <span>{filteredMuscle}</span>
-                            <X className='opacity-0 group-hover:opacity-100 transition-all hover:delay-100 text-muted-foreground scale-75 absolute right-0 mr-2' />
-                        </Badge>
-                    </motion.a>
-                )}
-                {filteredEquipment && (
-                    <motion.a
-                        initial={{ scale: 0.8, opacity: 0 }}
-                        animate={{ scale: 1, opacity: 1 }}
-                    >
-                        <Badge
-                            className='group  transition-all hover:pr-10 h-8 rounded-md text-sm px-4 gap-2 relative'
-                            variant={"secondary"}
-                            // onClick={() => setFilteredEquipment("")}
-                        >
-                            <span>{filteredEquipment}</span>
-                            <X className='opacity-0 group-hover:opacity-100 transition-all hover:delay-100 text-muted-foreground scale-75 absolute right-0 mr-2' />
-                        </Badge>
-                    </motion.a>
-                )}
-                {/* Add filter buttons */}
-                <Popover>
-                    <PopoverTrigger>
-                        <Button variant={"outline"} className='h-8 text-xs'>
-                            <ListFilter className='scale-75' /> Filtrar
-                        </Button>
-                    </PopoverTrigger>
-                    <PopoverContent align='start' side='right' className='w-auto bg-background p-2'>
-                        <div className='flex flex-col gap-[2px]'>
-                            <Button
-                                variant={"ghost"}
-                                className='h-8 justify-start gap-2 px-2'
-                                onClick={() => setMuscleFilterDrawerOpen(true)}
-                            >
-                                <Dumbbell className='text-muted-foreground scale-75' /> Músculos
-                            </Button>
-                            <hr></hr>
-                            <Button variant={"ghost"} className='h-8 justify-start gap-2 px-2'>
-                                <Triangle className='text-muted-foreground scale-75' /> Equipamentos
-                            </Button>
-                            <hr></hr>
-                            <Button variant={"ghost"} className='h-8 justify-start gap-2 px-2'>
-                                <Shapes className='text-muted-foreground scale-75' /> Dificuldade
-                            </Button>
-                        </div>
-                    </PopoverContent>
-                </Popover>
-            </div>
-            {/* Drawers */}
-            <MuscleFilterDrawer
-                muscles={muscles}
-                muscleFilterDrawerOpen={muscleFilterDrawerOpen}
-                setMuscleFilterDrawerOpen={setMuscleFilterDrawerOpen}
-                setFilteredMuscle={setFilteredMuscle}
-                filteredMuscle={filteredMuscle}
-                setCurrentFilters={setCurrentFilters}
+        <div className={className}>
+            {/* Filters */}
+            <FilterDrawer
+                drawerTitle='Grupos musculares'
+                drawerDescription='Selecione o grupo musculares para filtrar os exercícios.'
+                options={muscles}
+                filterKey={"muscle"}
+                allowAnyFilter={true}
                 currentFilters={currentFilters}
             />
-        </>
+        </div>
     );
 }
