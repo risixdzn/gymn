a3f8667cb68c7dc63349f1c1a5b5b1ca4b53d1c0
Ricardo Amorim <102877738+risixdzn@users.noreply.github.com>
2023-11-09 23:19:14 -0300

    :milky_way: `Workout` feat: Add rep / Delete exercises

--- components/Dashboard/Workouts/Exercise.tsx
+++ components/Dashboard/Workouts/Exercise.tsx
@@ -7,20 +7,33 @@ import {
     DialogDescription,
     DialogTitle,
 } from "@/components/ui/dialog";
-import { Info, MoreVertical, Plus } from "lucide-react";
-import { type Exercise } from "@/types/Workout";
+import { Info, MoreVertical, Plus, Shuffle, Trash } from "lucide-react";
+import { type Workout, type Exercise } from "@/types/Workout";
+import { AnimatePresence, motion } from "framer-motion";
 
+import {
+    DropdownMenu,
+    DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuLabel,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+import { UseFormSetValue, UseFormWatch } from "react-hook-form";
+import * as z from "zod";
 const ExerciseInfo = ({ exercise }: { exercise: Exercise }) => {
     return (
         <>
             <DialogTitle>{exercise.name}</DialogTitle>
             <DialogDescription className='-mt-2'>
                 <span className='flex gap-2 max-w-screen flex-wrap'>
-                    {exercise.muscles.map((muscle, index) => (
-                        <Badge key={index} className='rounded-md'>
-                            {muscle}
-                        </Badge>
-                    ))}
+                    {exercise && exercise.muscles
+                        ? exercise.muscles.map((muscle, index) => (
+                              <Badge key={index} className='rounded-md'>
+                                  {muscle}
+                              </Badge>
+                          ))
+                        : null}
                 </span>
             </DialogDescription>
             <hr></hr>
@@ -35,9 +48,39 @@ const ExerciseInfo = ({ exercise }: { exercise: Exercise }) => {
     );
 };
 
-export default function ExerciseDisplay({ exercise }: { exercise: Exercise }) {
+export default function ExerciseDisplay({
+    exercise,
+    index,
+    setterFn,
+    watch,
+}: {
+    exercise: Exercise;
+    setterFn: UseFormSetValue<z.infer<typeof Workout>>;
+    index: number;
+    watch: UseFormWatch<z.infer<typeof Workout>>;
+}) {
+    function deleteExercise(index: number) {
+        const previousExercises: Exercise[] = watch("exercises");
+        const filteredExercises = previousExercises.filter((_, i) => i !== index);
+        setterFn("exercises", filteredExercises);
+    }
+
+    function addSet(index: number) {
+        const previousExercises: Exercise[] = watch("exercises");
+        previousExercises[index].sets?.push({ variant: "Normal", load: 0, reps: 0 });
+        //console log previous exercises + the exercise with the new set
+        //the set should be like: {variant: "Normal", load: 0, reps: 0}
+        setterFn("exercises", previousExercises);
+    }
+
     return (
-        <div className='w-full h-auto rounded-md'>
+        <motion.div
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0, type: "" }}
+            transition={{ delay: index * 0.075 }}
+            exit={{ opacity: 0 }}
+            className='w-full h-auto rounded-md'
+        >
             <div id='header' className='w-full h-auto flex justify-between items-center'>
                 <div id='exercisename' className='flex items-center'>
                     <div
@@ -70,9 +113,29 @@ export default function ExerciseDisplay({ exercise }: { exercise: Exercise }) {
                     >
                         {exercise.level[0]}
                     </Badge>
-                    <Button size={"icon"} variant={"ghost"}>
-                        <MoreVertical className='' />
-                    </Button>
+                    {/* Options dropdown button */}
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button size={"icon"} variant={"ghost"}>
+                                <MoreVertical className='' />
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align='end'>
+                            <DropdownMenuLabel>Opções</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem className='flex items-center gap-2'>
+                                <Shuffle className='scale-75' />
+                                Reordenar{" "}
+                            </DropdownMenuItem>
+                            <DropdownMenuItem
+                                onClick={() => deleteExercise(index)}
+                                className='flex items-center gap-2 text-destructive'
+                            >
+                                <Trash className='scale-75' />
+                                Deletar{" "}
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>{" "}
+                    </DropdownMenu>{" "}
                 </div>
             </div>
             <table className='w-full mt-4'>
@@ -81,27 +144,36 @@ export default function ExerciseDisplay({ exercise }: { exercise: Exercise }) {
                         <th className='p-1 '>Série</th>
                         <th className='p-1'>Carga</th>
                         <th className='p-1'>Repetições</th>
+                        <th className='p-1'></th>
                     </tr>
                 </thead>
-                <tbody>
+                <tbody className='transition-all'>
                     {exercise.sets?.map((set, index) => (
-                        <tr
+                        <motion.tr
                             key={index}
+                            initial={{ opacity: 0, scaleY: 0, height: 0, transformOrigin: "top" }}
+                            animate={{
+                                opacity: 1,
+                                scaleY: 1,
+                                height: "auto",
+                                transformOrigin: "top",
+                            }}
+                            transition={{ ease: "easeInOut" }}
                             className='text-sm text-left odd:bg-transparent dark:even:bg-accent/20 even:bg-accent/60'
                         >
                             <th className='p-3 '>
                                 {set.variant == "Normal" ? index + 1 : set.variant}
                             </th>
                             <th className='p-3'>{set.load}</th>
-                            <th className='p-3'>{set.reps}</th>
-                        </tr>
+                            <th className='p-3'>{set.reps}</th>{" "}
+                        </motion.tr>
                     ))}
                 </tbody>
             </table>
-            <Button variant={"secondary"} className='w-full'>
+            <Button onClick={() => addSet(index)} variant={"secondary"} className='w-full'>
                 <Plus className='scale-75' />
                 Adicionar série
             </Button>
-        </div>
+        </motion.div>
     );
 }
